84daa23cea9339e9a958f7b164e102e6f7dd0468
Sergei Savoskin <graviton-exe@yandex.ru>
2023-03-30 02:10:51 +0300

    HW

--- HW_1/Program.cs
+++ HW_1/Program.cs
@@ -1,66 +1,31 @@
-﻿// Ввод чисел
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-/*
-Console.Clear();
-// int n = convert.ToInt32(Console.Readline());
-Console.WriteLine("Введите число");
-int n = int.Parse(Console.ReadLine()!);
-Console.WriteLine(n * 3);
-*/
-/*
-Программный код
-Hollo World!
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+
+Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет
+
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
 */
 
-// Спросить два числа и узнать какое больше
-// 1 > 2
-// 2 > 1
-// 2 = 1
-/*
+/*Задача № 2*/
 Console.Clear();
 Console.Write("Введите 1-е число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите 2-е число: ");
 int b = Convert.ToInt32(Console.ReadLine());
-// Интерполяция строк
-// Console.WriteLine($"Рузультат: {a} + {b} = {a + b}");
-// Проверка на равенство
-// Console.WriteLine(a > b);
-if (a > b)
-    Console.WriteLine(a);
-else if (a < b)
-    Console.WriteLine(b);
-else
-    Console.WriteLine("=");
-*/
-// for (begin; end; step)
-// i++ <=> i = i + 1
-
-/*
-for(int i = 1; i <= 10; i++)
-    Console.Write($"{i} ");
-Console.WriteLine();
-for(int i = 1; i <= 10; i+=2)
-    Console.Write($"{i} ");
-Console.WriteLine();
-for(int i = 10; i > 0; i--)
-    Console.Write($"{i} ");
-*/
- // Возвести число в квадрат
-
- 
-Console.Clear();
-Console.Write("Введите число 1: ");
-int a = int.Parse(Console.ReadLine()!);
-Console.Write("Введите число 2: ");
-int b = int.Parse(Console.ReadLine()!);
-int c = b * b;
-Console.WriteLine($"Квадрат числа 2 равен: {c}");
-
-if (a == c)
-    Console.WriteLine("Да!");
-
-else if (a < c)
-    Console.WriteLine("Первое число меньше квадрата второго");
-else if (a > c)
-Console.WriteLine($"Первое число равно {a} и явно больше квадрата второго {c}");
